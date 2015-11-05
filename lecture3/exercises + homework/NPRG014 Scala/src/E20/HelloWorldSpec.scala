6fec28cb7194a03953617bb3c1b7ffa5bf17646c
Tomas Bures <bures@d3s.mff.cuni.cz>
2015-11-06 00:10:28 +0100

    Updates to Scala 2.11

--- lecture3/exercises + homework/NPRG014 Scala/src/E20/HelloWorldSpec.scala
+++ lecture3/exercises + homework/NPRG014 Scala/src/E20/HelloWorldSpec.scala
@@ -7,7 +7,7 @@ class HelloWorldSpec extends Specification {
 
 	"The 'Hello world' string" should {
 		"contain 11 characters" in {
-			"Hello world" must have size(11)
+			"Hello world" must haveSize(11)
 		}
 		"start with 'Hello'" in {
 			"Hello world" must startWith("Hello")
